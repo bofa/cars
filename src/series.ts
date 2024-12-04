4e9846ba3250eeac5a22a816d5f3c113f51102f9
Per-Erik Karlsson <per-erik.karlsson@tietoevry.com>
2024-12-04 13:34:59 +0100

    smooth null as NaN

--- src/series.ts
+++ src/series.ts
@@ -4,14 +4,14 @@ export function smooth(list: { x: DateTime, y: number }[], size: number) {
   // TODO Control if it's correct
   const output = list
     .map((v1, i1, a1) => {
-      const y = a1
+      const Y = a1
         .slice(i1-size+1, i1+1)
         // .filter((v2, i2) => i2 <= i1 && v1.x.diff(v2.x.plus({ days: 3 }), 'months').months <= size - 1)
-        .map(({ y }, i, a) => y)
+        .map(({ y }, i, a) => y == null ? NaN : y)
 
       return {
         x: v1.x,
-        y: y.reduce((acc, v) => acc + v, 0)
+        y: Y.reduce((acc, y) => acc + y, 0)
       }
     })
     .slice(size - 1)
