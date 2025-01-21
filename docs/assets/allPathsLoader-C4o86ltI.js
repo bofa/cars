94591b288f93aa59ea53735db060f8fcbff2cb0d
Per-Erik Karlsson <per-erik.karlsson@tietoevry.com>
2025-01-21 10:15:25 +0100

    deploy

--- docs/assets/allPathsLoader-C4o86ltI.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./allPaths-BE90iRxx.js","./index-DBrX4SuH.js","./index-DeDKg4Un.js","./index-D1qwTphN.js","./index-C8k43UVs.css"])))=>i.map(i=>d[i]);
-import{_ as a,a as i,b as o}from"./index-D1qwTphN.js";var s=function(n,r){return a(void 0,void 0,void 0,function(){var e;return i(this,function(t){switch(t.label){case 0:return[4,o(()=>import("./allPaths-BE90iRxx.js"),__vite__mapDeps([0,1,2,3,4]),import.meta.url)];case 1:return e=t.sent().getIconPaths,[2,e(n,r)]}})})};export{s as allPathsLoader};
