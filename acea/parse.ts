b5b3f7b0a6d888e7e15c07cc042bce0e0e83bd4c
bofa <per-erik.karlsson@tietoevry.com>
2025-05-27 10:58:50 +0200

    Scrape april data

--- acea/parse.ts
+++ acea/parse.ts
@@ -35,8 +35,8 @@ const files = [
   // { date: '2024-12-01', file: './acea/2024-12.pdf' },
   // { date: '2025-01-01', file: './acea/2025-01.pdf' },
   // { date: '2025-02-01', file: './acea/2025-02.pdf' },
-  { date: '2025-03-01', file: './acea/2025-03.pdf' },
-
+  // { date: '2025-03-01', file: './acea/2025-03.pdf' },
+  { date: '2025-04-01', file: './acea/2025-04.pdf' },
 ]
 
 const rowOffset = 22
