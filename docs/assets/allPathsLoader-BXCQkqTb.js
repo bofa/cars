fdd3869062d8689d81f0250a10b379f9faff04ed
bofa <per-erik.karlsson@tietoevry.com>
2025-08-30 11:35:58 +0200

    deploy

---
+++ docs/assets/allPathsLoader-BXCQkqTb.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./allPaths-yDD_Ci_1.js","./index-03c8d4Gh.js","./index-PKNBViIU.js","./index-Du6b7BL2.js","./index-C2TjEiL5.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-Du6b7BL2.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-yDD_Ci_1.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]),import.meta.url);return o(t,a)};export{s as allPathsLoader};
