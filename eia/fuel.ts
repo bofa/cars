d9fc5834fec15f521d360902a552c8b9d929a260
bofa <per-erik.karlsson@tietoevry.com>
2025-10-06 15:14:21 +0200

    More fuel scrapes

--- eia/fuel.ts
+++ eia/fuel.ts
@@ -2,53 +2,56 @@ const fs = require('fs')
 import { DateTime } from 'luxon'
 
 [
-  // { "countryId": "ALB", "name": "Albania" },
-  // { "countryId": "AND", "name": "Andorra" },
-  // { "countryId": "ARM", "name": "Armenia" },
-  // { "countryId": "AUT", "name": "Austria" },
-  // { "countryId": "AZE", "name": "Azerbaijan" },
-  // { "countryId": "BGR", "name": "Bulgaria" },
-  // { "countryId": "BIH", "name": "Bosnia and Herzegovina" },
-  // { "countryId": "BLR", "name": "Belarus" },
-  // { "countryId": "BEL", "name": "Belgium" },
-  // { "countryId": "BGR", "name": "Bulgaria" },
-  // { "countryId": "CZE", "name": "Czechia" },
-  // { "countryId": "DNK", "name": "Denmark" },
-  // { "countryId": "EST", "name": "Estonia" },
-  // { "countryId": "FIN", "name": "Finland" },
-  // { "countryId": "FRA", "name": "France" },
-  // { "countryId": "GEO", "name": "Georgia" },
-  // { "countryId": "DEU", "name": "Germany" },
+  { "countryId": "ALB", "name": "Albania" },
+  { "countryId": "AND", "name": "Andorra" },
+  { "countryId": "ARM", "name": "Armenia" },
+  { "countryId": "AUT", "name": "Austria" },
+  { "countryId": "AZE", "name": "Azerbaijan" },
+  { "countryId": "BGR", "name": "Bulgaria" },
+  { "countryId": "BIH", "name": "Bosnia and Herzegovina" },
+  { "countryId": "BLR", "name": "Belarus" },
+  { "countryId": "BEL", "name": "Belgium" },
+  { "countryId": "BGR", "name": "Bulgaria" },
+  { "countryId": "CZE", "name": "Czechia" },
+  { "countryId": "DNK", "name": "Denmark" },
+  { "countryId": "EST", "name": "Estonia" },
+  { "countryId": "FIN", "name": "Finland" },
+  { "countryId": "FRA", "name": "France" },
+  { "countryId": "GEO", "name": "Georgia" },
+  { "countryId": "DEU", "name": "Germany" },
   { "countryId": "GRC", "name": "Greece" },
-  // { "countryId": "HUN", "name": "Hungary" },
-  // { "countryId": "ISL", "name": "Iceland" },
+  { "countryId": "HUN", "name": "Hungary" },
+  { "countryId": "ISL", "name": "Iceland" },
   { "countryId": "IRL", "name": "Ireland" },
-  // { "countryId": "ITA", "name": "Italy" },
-  // { "countryId": "KAZ", "name": "Kazakhstan" },
-  // { "countryId": "KOS", "name": "Kosovo" },
-  // { "countryId": "LTU", "name": "Lithuania" },
+  { "countryId": "ITA", "name": "Italy" },
+  { "countryId": "KAZ", "name": "Kazakhstan" },
+  { "countryId": "KOS", "name": "Kosovo" },
+  { "countryId": "LTU", "name": "Lithuania" },
   { "countryId": "LUX", "name": "Luxembourg" },
-  // { "countryId": "LVA", "name": "Latvia" },
-  // { "countryId": "MDA", "name": "Moldova" },
-  // { "countryId": "MKD", "name": "North Macedonia" },
-  // { "countryId": "MNE", "name": "Montenegro" },
-  // { "countryId": "MCO", "name": "Monaco" },
-  // { "countryId": "NLD", "name": "Netherlands" },
-  // { "countryId": "NOR", "name": "Norway" },
+  { "countryId": "LVA", "name": "Latvia" },
+  { "countryId": "MDA", "name": "Moldova" },
+  { "countryId": "MKD", "name": "North Macedonia" },
+  { "countryId": "MNE", "name": "Montenegro" },
+  { "countryId": "MCO", "name": "Monaco" },
+  { "countryId": "NLD", "name": "Netherlands" },
+  { "countryId": "NOR", "name": "Norway" },
   { "countryId": "POL", "name": "Poland" },
   { "countryId": "PRT", "name": "Portugal" },
   { "countryId": "ROU", "name": "Romania" },
-  // { "countryId": "RUS", "name": "Russia" },
-  // { "countryId": "SRB", "name": "Serbia" },
-  // { "countryId": "SVK", "name": "Slovakia" },
+  { "countryId": "RUS", "name": "Russia" },
+  { "countryId": "SRB", "name": "Serbia" },
+  { "countryId": "SVK", "name": "Slovakia" },
   { "countryId": "SVN", "name": "Slovenia" },
-  // { "countryId": "ESP", "name": "Spain" },
-  // { "countryId": "SWE", "name": "Sweden" },
-  // { "countryId": "CHE", "name": "Switzerland" },
-  // { "countryId": "TUR", "name": "Turkey" },
-  // { "countryId": "UKR", "name": "Ukraine" },
+  { "countryId": "ESP", "name": "Spain" },
+  { "countryId": "SWE", "name": "Sweden" },
+  { "countryId": "CHE", "name": "Switzerland" },
+  { "countryId": "TUR", "name": "Turkey" },
+  { "countryId": "UKR", "name": "Ukraine" },
   { "countryId": "GBR", "name": "United Kingdom" },
-].reverse().forEach(({ countryId, name }, countryIndex) =>
+  { "countryId": "MLT", "name": "Malta" },
+].reverse().forEach(async ({ countryId, name }, countryIndex) => {
+  await sleep(100*countryIndex, null)
+
   Promise.all([
     {
       key: 'gasoline',
@@ -89,4 +92,12 @@ import { DateTime } from 'luxon'
         // }))
       })
       .catch(error => console.error('countryId', error))
-  )
+})
+
+function sleep<T>(ms: number, resolved: T) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(resolved)
+    }, ms)
+  })
+}
