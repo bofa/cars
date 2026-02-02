2f45113e03a5309858bf962628aab47c73352184
bofa <per-erik.karlsson@tietoevry.com>
2026-02-02 22:01:01 +0100

    deploy

---
+++ docs/assets/allPathsLoader-BqUiCVXM.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./allPaths-C9zuPZEm.js","./index-BmDjQHI0.js","./index-CzNKtOwn.js","./index-uzQK5ose.js","./index-B10jOcl1.css"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-uzQK5ose.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-C9zuPZEm.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]),import.meta.url);return o(t,a)};export{s as allPathsLoader};
--- docs/assets/allPathsLoader-DYXt6Lq6.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./allPaths-CuqPRRBt.js","./index-BmDjQHI0.js","./index-CzNKtOwn.js","./index-BHwbWiP9.js","./index-B10jOcl1.css"])))=>i.map(i=>d[i]);
-import{_ as e}from"./index-BHwbWiP9.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-CuqPRRBt.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4]),import.meta.url);return o(t,a)};export{s as allPathsLoader};
